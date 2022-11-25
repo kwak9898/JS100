f44b9fb6e394801172131278a4f4d9252dc8b73c
kwak9898 <kwak9898@gitbub.com>
2022-11-25 14:31:41 +0900

    사용할 수 없는 변수면

--- chapter1/7.js
+++ chapter1/7.js
@@ -1,5 +1,5 @@
-const age = 1;
-const Age = 1;
-const let = 1;
-const _age = 1;
-const 1age = 1;
+const age = 1;  // O
+const Age = 1;  // O
+const let = 1;  // Javascript에는 예약어라는게 있는데 let은 변수 할당에 사용하므로 사용이 불가하다.
+const _age = 1; // _, $만 사용 가능
+const 1age = 1; // 변수명에 숫자를 붙힐 수 없다.
