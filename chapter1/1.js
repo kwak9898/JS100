df791deaebfd453da6edd3db0741d85867223bc8
kwak9898 <kwak9898@gitbub.com>
2022-11-25 11:37:10 +0900

    문제 추가

--- chapter1/1.js
+++ chapter1/1.js
@@ -1,6 +1,6 @@
 let nums = [100, 200, 300, 400, 500];
 
-nums.pop();
-nums.pop();
+nums.splice(3, 2);
+// splice로 배열 3번째부터 2개를 삭제하여 원하는 값 출력
 
 console.log(nums);
---
+++ chapter1/2.js
@@ -0,0 +1,6 @@
+let arr = [200, 100, 300];
+
+arr.splice(2, 0, 10000);
+// splice를 이용해서 배열의 2번째의 숫자 10000을 추가
+
+console.log(arr);
