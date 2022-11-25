c95e977ac2f8a722f7faa0072ae53d4dd196653a
kwak9898 <kwak9898@gitbub.com>
2022-11-25 16:35:05 +0900

    평균 점수

--- chapter1/18.js
+++ chapter1/18.js
@@ -1,10 +1,19 @@
-const score = [20, 30, 50];
+// const score = [20, 30, 50];
+// let sum = 0;
+//
+// for (let i = 0; i < score.length; i++) {
+//     sum += score[i];
+// }
+//
+// const answer = sum / score.length;
+//
+// console.log(Math.floor(answer));
+
+const score = '20 30 40'.split(' ');
 let sum = 0;
 
 for (let i = 0; i < score.length; i++) {
-    sum += score[i];
+    sum += parseInt(score[i]);
 }
 
-const answer = sum / score.length;
-
-console.log(Math.floor(answer));
+console.log(Math.floor(sum / score.length));
