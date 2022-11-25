5a311a7cf75fbfc1d2ca15338402226957eb86d2
kwak9898 <kwak9898@gitbub.com>
2022-11-25 14:50:50 +0900

    concat을 활용한 출력 방법

---
+++ chapter1/9.js
@@ -0,0 +1,10 @@
+let year = '2022';
+let month = '11';
+let day = '23';
+let hour = '14';
+let minute = '43';
+let second = '37';
+
+let result = year.concat("/", month, "/", day, " ", hour, ":", minute, ":", second);
+
+console.log(result);
